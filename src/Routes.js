81fd51cbb598216131fcb37cd1ddb0efb1caa49e
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-13 23:26:44 +1100

    took out practice route

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
-import FAQ from './components/FAQ';
 import App from './components/App';
 
 
 const Routes = (
   <Router>
     <div>
-      {/* <Route exact path="/jose" user="7" component={ App } /> */}
       <Route exact path="/jose" render={props => <App user_id="7" {...props} />} />
       <Route exact path="/maddi" render={props => <App user_id="8" {...props} />} />
       <Route exact path="/craigsy" render={props => <App user_id="9" {...props} />} />
-      {/* <Route exact path="/abc" render={props => <TestWidget someProp="2" {...props} />} /> */}
-      {/* <Route exact path="/faq" component={ FAQ } /> */}
     </div>
   </Router>
 )
