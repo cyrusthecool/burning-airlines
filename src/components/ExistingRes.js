d3f3ec32a16bbd73a0a71f81de967211fb759f43
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-14 13:01:32 +1100

    cancel status and no res conditional

--- src/components/ExistingRes.js
+++ src/components/ExistingRes.js
@@ -2,12 +2,22 @@ import React, { PureComponent as Component } from 'react';
 import axios from 'axios';
 
 class ExistingRes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: ""
+    }
+  }
 
   _handleClick = (res_id,array_res) => {
     console.log( res_id );
     console.log(array_res);
 
-    axios.delete(`http://burningairlinesdb.herokuapp.com/reservations/${res_id.id}`).then((res)=>{console.log(res)})
+    axios.delete(`http://burningairlinesdb.herokuapp.com/reservations/${res_id.id}`).then(
+        this.setState({
+          status: "Reservation successfully cancelled."
+        })
+    );
 
 
 
@@ -20,22 +30,28 @@ class ExistingRes extends Component {
 
       let fio= this.props.res.reservations;
       // console.log(fio);
-      return (
-        <div>
-          <p>Do you have existing bookings? If you do, a list of them will appear below.</p>
-          {fio.map (s=> <p key={s.id}>{s.flight.date} Flight {s.flight.number} - {s.flight.origin} to {s.flight.destination}, Seat {s.seat}    <button className="res-button book" onClick = { () => { this._handleClick(s,fio) } }>
-              Cancel Reservation
-            </button></p>
+      if (fio.length > 0){
+        return (
+          <div>
+            <p>MY RESERVATIONS</p>
+            {fio.map (s=> <p key={s.id}><span className="flight-span">{s.flight.date} Flight {s.flight.number} - {s.flight.origin} to {s.flight.destination}, Seat {s.seat}    </span><button className="res-button book" onClick = { () => { this._handleClick(s,fio) } }>Cancel Reservation</button></p>
 
-          )}
-        </div>
-    );
+            )}
+            <p className="res-status">{this.state.status}</p>
+          </div>
+        );
+      }
+      else {
+        return(
+          <div>No current reservations</div>
+        );
+      }
     }
     else {
 
       // console.log(this);
       return (
-        <div></div>
+        <div>No current reservations</div>
       )
     }
   }
