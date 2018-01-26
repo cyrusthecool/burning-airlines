1b30da8e5e5000ea5edb51d816d7d1d511b36bdd
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-27 01:11:39 +1100

    address deploy errors re mixed content & serviceworkers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import Routes from './Routes';
+
+import { unregister } from './registerServiceWorker';
+unregister();
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(Routes, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
